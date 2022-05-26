76262dc4ec3b98295eb61d4015d99938e46c70d7
nikGogolev <nv_gogolev@mail.ru>
2022-05-26 22:17:06 +0300

    Lesson2 HW

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { renderSearchFormBlock } from './search-form.js'
+import { callback, collectSearchParams, renderSearchFormBlock, search } from './search-form.js'
 import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock } from './user.js'
+import { getFavoritesAmount, getUserData, renderUserBlock } from './user-component.js'
 import { renderToast } from './lib.js'
 
 window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('Nikita', 'https://avatars.githubusercontent.com/u/78842306?v=4', 1)
+
+  renderUserBlock(getUserData(), getFavoritesAmount());
   renderSearchFormBlock()
   renderSearchStubBlock()
   renderToast(
     { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
     { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
   )
+  document.getElementById('search-form-block').addEventListener('submit', (e) => {
+    e.preventDefault();
+    search(collectSearchParams(), callback);
+  })
 })
