64004c3c950d110c6e839b6930d0070693bf2a5e
nikGogolev <nv_gogolev@mail.ru>
2022-06-07 15:50:13 +0300

    Lesson1 HW1_1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import { renderSearchFormBlock } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock } from './user.js'
-import { renderToast } from './lib.js'
+import { renderSearchFormBlock } from "./search-form.js";
+import { renderSearchStubBlock } from "./search-results.js";
+import { renderUserBlock } from "./user.js";
+import { renderToast } from "./lib.js";
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('Nikita', 'https://avatars.githubusercontent.com/u/78842306?v=4', 1)
-  renderSearchFormBlock()
-  renderSearchStubBlock()
+window.addEventListener("DOMContentLoaded", () => {
+  renderUserBlock(
+    "Nikita",
+    "https://avatars.githubusercontent.com/u/78842306?v=4",
+    1
+  );
+  renderSearchFormBlock();
+  renderSearchStubBlock();
   renderToast(
-    { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
-    { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
-  )
-})
+    {
+      text: "Это пример уведомления. Используйте его при необходимости",
+      type: "success",
+    },
+    {
+      name: "Понял",
+      handler: () => {
+        console.log("Уведомление закрыто");
+      },
+    }
+  );
+});
--- src/lib.ts
+++ src/lib.ts
@@ -1,32 +1,39 @@
 export function renderBlock(elementId, html) {
-  const element = document.getElementById(elementId)
-  element.innerHTML = html
+  const element = document.getElementById(elementId);
+  element.innerHTML = html;
 }
 
-export function renderToast(message, action?) {
-  let messageText = ''
+interface toastMessage {
+  text: string;
+  type: string;
+}
+
+interface toastAction {
+  name: string;
+  handler: () => void;
+}
+
+export function renderToast(message: toastMessage, action?: toastAction): void {
+  let messageText = "";
 
   if (message != null) {
     messageText = `
       <div id="info-block" class="info-block ${message.type}">
         <p>${message.text}</p>
-        <button id="toast-main-action">${action?.name || 'Закрыть'}</button>
+        <button id="toast-main-action">${action?.name || "Закрыть"}</button>
       </div>
-    `
+    `;
   }
 
-  renderBlock(
-    'toast-block',
-    messageText
-  )
+  renderBlock("toast-block", messageText);
 
-  const button = document.getElementById('toast-main-action')
+  const button = document.getElementById("toast-main-action");
   if (button != null) {
     button.onclick = function () {
       if (action != null && action.handler != null) {
-        action.handler()
+        action.handler();
       }
-      renderToast(null)
-    }
+      renderToast(null);
+    };
   }
 }
